1aafe3586457219fe264aaf4858edba2c450536b
Michael Hoffer <info@michaelhoffer.de>
2017-10-28 15:02:20 +0200

    Updated plugin template to JDK 9 compatible VRL version

--- VRL-Tutorial-Plugin-01/src/main/java/edu/gcsc/vrl/tutorial/TutorialPluginConfigurator.java
+++ VRL-Tutorial-Plugin-01/src/main/java/edu/gcsc/vrl/tutorial/TutorialPluginConfigurator.java
@@ -1,6 +1,29 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2017 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Michael Hoffer <info@michaelhoffer.de> "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Michael Hoffer <info@michaelhoffer.de> OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Michael Hoffer <info@michaelhoffer.de>.
  */
 package edu.gcsc.vrl.tutorial;
 
@@ -56,8 +79,8 @@ public class TutorialPluginConfigurator extends VPluginConfigurator{
            //
            // To ensure compatibility with future versions of VRL,
            // you should only use the vapi or api object for registration.
-           // If you directly use the canvas or its properties, please make
-           // sure that you specify the VRL versions you are compatible with
+           // If you directly use the canvas or its properties, please make sure
+           // that you specify the VRL versions your plugin is compatible with
            // in the constructor of this plugin configurator because the
            // internal api is likely to change.
            //
